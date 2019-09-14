4f1c215839fb8ddd7cc84de8fac43bda2882e25e
Peter Geyser <peterpgeyser@gmail.com>
2019-09-13 20:14:26 -0700

    Added exit function to bamazonCustomer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,7 +17,6 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     customerApp();
-
 });
 
 //main app
@@ -25,78 +24,93 @@ function customerApp() {
     connection.query("SELECT * FROM productsTB", function (err, results) {
         if (err) throw err;
 
-        //Inquirer prompt to display in console
-        inquirer
-            .prompt([
+        //Inquirer prompt
+        inquirer.prompt([
 
                 //List of items available
                 {
                     name: "choice",
                     type: "rawlist",
+
+                    //function to grab choices from sql db
                     choices: function () {
                         var choiceArray = [];
                         for (var i = 0; i < results.length; i++) {
+
+                            //Adding choice and item price to array as a string
                             choiceArray.push(
                                 results[i].product_name +
                                 " || Price: $" +
                                 results[i].price
                             );
                         }
+                        choiceArray.push("Exit");
                         return choiceArray;
                     },
                     message: "What item would you like to purchase?"
                 },
-
-                //determining desired quantity
-                {
-                    name: "quantity",
-                    type: "input",
-                    message: "How much would you like to buy?"
-                }
             ])
             .then(function (answer) {
-                // get the information of the chosen item
-                var choice = answer.choice;
 
-                //replace all characters after "|" with empty string and then trim spaces
-                choice = choice.replace(/\|.*/,'').trim();
-                console.log(choice);
+                //Stop function if user wants to exit
+                if (answer.choice === "Exit") {
+                    connection.end();
 
-                var chosenItem;
-                for (var i = 0; i < results.length; i++) {
-                    if (results[i].product_name === choice) {
-                        chosenItem = results[i];
+                //Continue on with application    
+                } else {
+
+                    // get the information of the chosen item
+                    var choice = answer.choice;
+
+                    //replace all characters after "|" with empty string and then trim spaces so that we only have the name of the item
+                    choice = choice.replace(/\|.*/, '').trim();
+
+                    //Making a chosen item variable that is set to the object grabbed from the database
+                    var chosenItem;
+                    for (var i = 0; i < results.length; i++) {
+                        if (results[i].product_name === choice) {
+                            chosenItem = results[i];
+                        }
                     }
-                }
+                    inquirer.prompt(
+                        //determining desired quantity
+                        {
+                            name: "quantity",
+                            type: "input",
+                            message: "How much would you like to buy?"
+                        }
+                    ).then(function (answer) {
 
-                // console.log(chosenItem);
+                        // determine if there is sufficient stock
+                        if (chosenItem.stock > parseInt(answer.quantity)) {
 
-                // determine if there is sufficient stock
-                if (chosenItem.stock > parseInt(answer.quantity)) {
+                            var updatedQuantity = (chosenItem.stock - parseInt(answer.quantity));
 
-                    var updatedQuantity = (chosenItem.stock - parseInt(answer.quantity));
-
-                    // There was sufficient stock, update db, let the user know, and end process
-                    var query = "UPDATE productsTB SET ? WHERE ?"
-                    connection.query(query,
-                        [{
-                                stock: updatedQuantity
-                            },
-                            {
-                                item_id: chosenItem.item_id
-                            }
-                        ],
-                        function (error) {
-                            if (error) throw error;
-                            console.log("Item purchased successfully!" + "\nTotal cost: $" + (parseInt(answer.quantity) * chosenItem.price).toFixed(2));
+                            // There was sufficient stock, update db, let the user know, and end process
+                            var query = "UPDATE productsTB SET ? WHERE ?"
+                            connection.query(query,
+                                [{
+                                        stock: updatedQuantity
+                                    },
+                                    {
+                                        item_id: chosenItem.item_id
+                                    }
+                                ],
+                                function (error) {
+                                    if (error) throw error;
+                                    console.log("Item purchased successfully!" + "\nTotal cost: $" + (parseInt(answer.quantity) * chosenItem.price).toFixed(2));
+                                    connection.end();
+                                }
+                            );
+                        } else {
+                            // Insufficient stock, so end process
+                            console.log("Stock: " + chosenItem.stock + "\nInsufficient stock, please try again");
                             connection.end();
                         }
-                    );
-                } else {
-                    // Insufficient stock, so end process
-                    console.log("Stock: " + chosenItem.stock + "\nInsufficient stock, please try again");
-                    connection.end();
+                    })
                 }
+
+
             })
     })
 }
