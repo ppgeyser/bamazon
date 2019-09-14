1b8b27cdba4c9137aa0561d6af8bef741197cab6
Peter Geyser <peterpgeyser@gmail.com>
2019-09-13 20:39:54 -0700

    productView function complete

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,5 +17,58 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     managerApp();
-
 });
+
+//main app
+function managerApp() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View Products For Sale",
+            "View Low Inventory",
+            "Add to Inventory",
+            "Add New Product",
+            "Exit"
+        ]
+    }).then(function (answer) {
+        switch (answer.action) {
+            case "View Products For Sale":
+                productView();
+                break;
+
+            case "View Low Inventory":
+                productViewLow();
+                break;
+
+            case "Add to Inventory":
+                inventoryAdd();
+                break;
+
+            case "Add New Product":
+                productAdd();
+                break;
+
+            case "Exit":
+                connection.end();
+                break;
+        }
+    })
+}
+
+function productView() {
+    connection.query("SELECT * FROM productsTB", function (err, results) {
+        if (err) throw err;
+
+        for (var i = 0; i < results.length; i++) {
+            console.log(
+                results[i].item_id + 
+                ") " + results[i].product_name +
+                " | Price: " + results[i].price + 
+                " | Quantity: " + results[i].stock
+                )
+        }
+        connection.end();
+    })
+}
